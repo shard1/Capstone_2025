96c7d4335dd410b4a7cf6137b254e76d6401700c
San Sung <sansungp@gmail.com>
2025-04-04 15:00:00 +0900

    First commit

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 import os
 import torch
 
+def train():
+    pass
+
 def val():
     pass
-    
+
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
